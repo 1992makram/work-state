b9f750ae88c67366c468879ebb6bc546d12a63f9
1992makram <makramkrayen@gmail.com>
2022-02-24 11:58:53 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state={person:{
+    fullName:"makram",
+    bio:"no thiing",
+     imgSrc:"image",
+      profession:"developper",
+  },
+  show:true,
+  timer:0
+};
+  handleShowperson=()=>{
+ this.setState({show:!this.state.show});
+  };
+  componentDidMount(){
+   setInterval(()=>this.setState({timer:this.state.timer+1}),1000)
+  }
+  
+  
+  render() {
+    const mystyle={
+      color: "white",
+      backgroundColor: "DodgerBlue",
+    }
+    const mstyle={
+      color: "white",
+      backgroundColor: "red",
+      
+    }
+    return (
+      <>
+      
+      
+      <button onClick={this.handleShowperson}>click</button>
+      {this.state.show ?(
+        <>
+        <h1 style={mystyle}>{this.state.person.fullName}</h1>
+        <h2 style={mystyle}>{this.state.person.bio}</h2>
+        <h3 style={mstyle}>{this.state.person.imgSrc}</h3>
+        <h4 style={mstyle}>{this.state.person.profession}</h4>
+      </>  
+      ):null}
+      <p>{this.state.timer}</p>
+      </>
+    );
+  }
 }
 
-export default App;
